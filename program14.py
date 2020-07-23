8de71a0b087be150baf292aafd7edc4a40c89d16
Aman Shukla <66578261+Aman-Shukla-18@users.noreply.github.com>
2020-07-23 20:22:23 +0530

    Create program14.py

---
+++ program14.py
@@ -0,0 +1,108 @@
+''' Digit Pairs
+Problem Description
+Given N three-digit numbers, your task is to find bit score of all N numbers and then print the number of pairs possible based on these calculated bit score.
+
+1. Rule for calculating bit score from three digit number:
+
+From the 3-digit number,
+
+· extract largest digit and multiply by 11 then
+
+· extract smallest digit multiply by 7 then
+
+· add both the result for getting bit pairs.
+
+Note: - Bit score should be of 2-digits, if above results in a 3-digit bit score, simply ignore most significant digit.
+
+Consider following examples:
+
+Say, number is 286
+
+Largest digit is 8 and smallest digit is 2
+
+So, 8*11+2*7 =102 so ignore most significant bit , So bit score = 02.
+
+Say, Number is 123
+
+Largest digit is 3 and smallest digit is 1
+
+So, 3*11+7*1=40, so bit score is 40.
+
+2. Rules for making pairs from above calculated bit scores
+
+Condition for making pairs are
+
+· Both bit scores should be in either odd position or even position to be eligible to form a pair.
+
+· Pairs can be only made if most significant digit are same and at most two pair can be made for a given significant digit.
+
+Constraints
+N<=500
+
+Input Format
+First line contains an integer N, denoting the count of numbers.
+
+Second line contains N 3-digit integers delimited by space
+
+Output
+One integer value denoting the number of bit pairs.
+
+Timeout
+1
+
+Explanation
+Example 1
+
+Input
+
+8 234 567 321 345 123 110 767 111
+
+Output
+
+3
+
+Explanation
+
+After getting the most and least significant digits of the numbers and applying the formula given in Rule 1 we get the bit scores of the numbers as:
+
+58 12 40 76 40 11 19 18
+
+No. of pair possible are 3:
+
+40 appears twice at odd-indices 3 and 5 respectively. Hence, this is one pair.
+
+12, 11, 18 are at even-indices. Hence, two pairs are possible from these three-bit scores.
+
+Hence total pairs possible is 3'''
+
+N = int(input())
+digit = input().split()
+bit_score = []
+for i in digit:
+    lst = list(map(int , i))
+    bit = (max(lst)*11)+(min(lst)*7)
+    if bit < 99:
+        for j in range(2):
+            x = bit%10
+            bit -= x
+            bit = bit/10
+
+        bit_score.append(int(x))
+
+    else:
+        bit_score.append(int((str(bit)[1:])[0]))## or use bit_score.append(bit%100)
+
+def OddEven(no):
+    if no%2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+
+pair = 0
+for i in range(len(bit_score)):
+    for j in range(i+1,len(bit_score)):
+        if bit_score[i] == bit_score[j]:
+            if OddEven(i) == OddEven(j):
+                pair += 1
+                break
+print(pair)
